4b2cc73139170f8f1bf11ba77a3084e5e8f8513d
K M Merajul Arefin <merajularefin@gmail.com>
2025-01-29 13:49:36 +0600

    Structure project in the correct manner

--- app/src/main/java/com/example/easyquran/domain/Chapter.kt
+++
@@ -1,9 +0,0 @@
-package com.example.easyquran.domain
-
-data class Chapter(
-    val id: Int,
-    val name: String,
-    val translatedName: String,
-    val versesCount: Int,
-    val revelationPlace: String
-)
