d7955161241cb05ab9ee8df72500ee25ce51a8df
K M Merajul Arefin <merajularefin@gmail.com>
2025-01-29 13:22:57 +0600

    Implement chapter list screen
    - Used retrofit to get the list from the api
    - Added composable to show the screen with minor details

--- app/src/main/java/com/example/easyquran/MainActivity.kt
+++ app/src/main/java/com/example/easyquran/MainActivity.kt
@@ -4,44 +4,37 @@ import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
 import androidx.activity.enableEdgeToEdge
+import androidx.activity.viewModels
 import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.padding
 import androidx.compose.material3.Scaffold
-import androidx.compose.material3.Text
-import androidx.compose.runtime.Composable
+import androidx.compose.runtime.getValue
+import androidx.compose.runtime.livedata.observeAsState
 import androidx.compose.ui.Modifier
-import androidx.compose.ui.tooling.preview.Preview
+import com.example.easyquran.ui.chapters.ChaptersList
+import com.example.easyquran.ui.chapters.viewmodels.ChaptersViewModel
 import com.example.easyquran.ui.theme.EasyQuranTheme
 
 class MainActivity : ComponentActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         enableEdgeToEdge()
+
+        val viewModel: ChaptersViewModel by viewModels()
+
         setContent {
+            val isLoading by viewModel.isLoading.observeAsState(true)
+            val chapters by viewModel.chaptersList.observeAsState(emptyList())
+
             EasyQuranTheme {
                 Scaffold(modifier = Modifier.fillMaxSize()) { innerPadding ->
-                    Greeting(
-                        name = "Android",
-                        modifier = Modifier.padding(innerPadding)
+                    ChaptersList(
+                        chapters = chapters,
+                        isLoading = isLoading,
+                        modifier = Modifier.fillMaxSize().padding(innerPadding)
                     )
                 }
             }
         }
-    }
-}
-
-@Composable
-fun Greeting(name: String, modifier: Modifier = Modifier) {
-    Text(
-        text = "Hello $name!",
-        modifier = modifier
-    )
-}
-
-@Preview(showBackground = true)
-@Composable
-fun GreetingPreview() {
-    EasyQuranTheme {
-        Greeting("Android")
     }
 }
